e3dd499f4eef37bfd8c46b65508719cdef01a122
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-01-31 13:08:03 -0500

    flask added

---
+++ app.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement=Base.classes.measurement
+Station=Base.classes.station
+
+joined_by_station=session.query(Measurement, Station).filter(Measurement.station == Station.station)
+for record in joined_by_station:
+    (Measurement, Station) = record
+    print (Measurement.station)
+    print (Station.station)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/stations<br/>"
+    )
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
