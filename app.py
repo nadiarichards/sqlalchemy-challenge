845f92b0d27790e389f7199298a6f17f85806d68
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-02-04 12:14:31 -0500

    temp start and end

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ Base.prepare(engine, reflect=True)
 Measurement=Base.classes.measurement
 Station=Base.classes.station
 
-
 #################################################
 # Flask Setup
 #################################################
@@ -42,28 +41,14 @@ def welcome():
         f"/api/v1.0/precipitation",
         f"/api/v1.0/stations"
     )
-# session = Session(engine)
-# for row in session.query(Measurement).limit(10).all():
-#     print(row.station, row.date)
-# session.close()
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Create our session (link) from Python to the DB
+
     session = Session(engine)
     query = session.query(Measurement.date, Measurement.prcp).all()
-    # Convert list of tuples into normal list
-    #all_prcp = {results_date : results_prcp}
 
     session.close()
-
-    # all_prcp = []
-    # for date, prcp in results:
-    #     prcp_dict = {}
-    #     prcp_dict["date"] = date
-    #     prcp_dict["prcp"] = precipitation
-    #     all_prcp.append(prcp_dict)
-
     return jsonify(query)
 
 @app.route("/api/v1.0/stations")
@@ -97,13 +82,50 @@ def tobs():
     return jsonify(most_active_station, temp_last_year)
 
 
-@app.route("/api/v1.0/,<start>")
-def start():
+@app.route("/api/v1.0/<start>/<end>")
+def date_range(start=none, end=none):
+
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
 
     session=Session(engine)
-    joined_by_station=session.query(Measurement, Station).filter(Measurement.station == Station.station)
-    for record in joined_by_station:
-        (measurement, station) = record
+
+    def stats(start=None, end=None):
+    """Return TMIN, TAVG, TMAX."""
+    # Select statement
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        # calculate TMIN, TAVG, TMAX for dates greater than start
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        # Unravel results into a 1D array and convert to a list
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    # calculate TMIN, TAVG, TMAX with start and stop
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+    # session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(
+    # Measurement.station=='USC00519281').all()
+
+
+    # joined_by_station=session.query(Measurement, Station).filter(Measurement.station == Station.station)
+    # for record in joined_by_station:
+    #     (measurement, station) = record
+
+    #     canonicalized = superhero.replace(" ", "").lower()
+    # for character in justice_league_members:
+    #     search_term = character["superhero"].replace(" ", "").lower()
+
+    #     if search_term == canonicalized:
+    #         return jsonify(character)
+
+
     session.close()
 
 if __name__ == '__main__':
