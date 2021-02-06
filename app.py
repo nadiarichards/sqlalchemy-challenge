a510d1c8158bf2407bbb8410b5a5caafb35ca094
nadiarichards <73898387+nadiarichards@users.noreply.github.com>
2021-02-05 22:08:08 -0500

    date error message added

--- app.py
+++ app.py
@@ -41,7 +41,8 @@ def welcome():
         f"/api/v1.0/precipitation<br>"
         f"/api/v1.0/stations<br>"
         f"/api/v1.0/tobs<br>"
-        f"/api/v1.0/start/end<br>"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end><br>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -49,13 +50,11 @@ def precipitation():
 
     session = Session(engine)
     query = session.query(Measurement.date, Measurement.prcp).all()
-
     session.close()
     return jsonify(query)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Create our session (link) from Python to the DB
     session = Session(engine)
     query = session.query(Measurement.station).all()
     session.close()
@@ -66,14 +65,11 @@ def stations():
 def tobs():
 
     session=Session(engine)
-
 # Query the dates and temperature observations of the most active station for the last year of data.
-
     session.query(Measurement.station, func.count(Measurement.station)).group_by(
     Measurement.station).order_by(func.count(Measurement.station).desc()).first()
     most_active_station=session.query(Measurement.tobs, Measurement.date).filter(
     Measurement.station=='USC00519281').all()
-
 # Return a JSON list of temperature observations (TOBS) for the previous year.
     latest_date=session.query(Measurement.date).order_by(Measurement.date.desc()).first()
     last_year=dt.date(2017,8,23)-dt.timedelta(days=365)
@@ -81,69 +77,22 @@ def tobs():
     session.close()
     return jsonify(most_active_station, temp_last_year)
 
-# @app.route("/api/v1.0/temp/<date_string>")
-# def to_date(date_string): 
-#     try:
-#         return dt.strptime(dateString, "%Y-%m-%d").date()
-#     except ValueError:
-#         raise ValueError('{} is not valid date in the format YYYY-MM-DD'.format(date_string))
-
-# @app.route("/api/v1.0/temp/<start>")
-
-# def stats(start=None, end=None):
-
-# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
-
-
-    # # Select statement
-    # sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-    # if not end:
-    #     # calculate TMIN, TAVG, TMAX for dates greater than start
-    #     results = session.query(*sel).filter(Measurement.date >= start).all()
-    #     # Unravel results into a 1D array and convert to a list
-    #     temps = list(np.ravel(results))
-    #     return jsonify(temps)
-    # # calculate TMIN, TAVG, TMAX with start and stop
-    # end_results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-    # # Unravel results into a 1D array and convert to a list
-    # end_temps = list(np.ravel(end_results))
-    # return jsonify(temps=end_temps)
-
-# @app.route("/api/v1.0/temp/<start>/<end>")
-# def temperature(start=None, end=None):
-#     session=Session(engine)
-#     latest_date=session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-#     # last_year=dt.date(2017,8,23)-dt.timedelta(days=365)
-#     # temp_last_year=session.query(Measurement.tobs).filter(Measurement.date >=last_year).all()
-#     #when given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive'''
-#     if end != None:
-#         temps = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-#                 filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-#     #when given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date
-#     else:
-#         temps = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-#                 filter(Measurement.date >= start).all()
-#         #convert list of tuples into normal list
-#     temps_rav = list(np.ravel(temps))
-#     #return json representation of the list
-#     session.close()
-#     return jsonify(temps)
-
 
 @app.route("/api/v1.0/<start>")
 def start(start):
-    session=Session(engine)
-    sel =[Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-    after_start = session.query(*sel).filter(Measurement.date >= start).group_by(Measurement.date).all()
+    if start not in Measurement.date:
+        print("Pick a date before 2017-08-23")
+    else:
+        session=Session(engine)
+        sel =[Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+        after_start = session.query(*sel).filter(Measurement.date >= start).group_by(Measurement.date).all()
         # Convert List of Tuples Into Normal List
-    start_list = list(after_start)
+        start_list = list(after_start)
         # Return JSON List of Min Temp, Avg Temp and Max Temp for a Given Start Range
-    session.close()
-    return jsonify(start_list)
+        session.close()
+        return jsonify(start_list)
 
-# Start-End Day Route
+
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
     session=Session(engine)
@@ -155,22 +104,6 @@ def start_end(start, end):
         # Return JSON List of Min Temp, Avg Temp and Max Temp for a Given Start-End Range
     session.close()
     return jsonify(start_end_day_list)
-
-
-    # session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(
-    # Measurement.station=='USC00519281').all()
-
-
-    # joined_by_station=session.query(Measurement, Station).filter(Measurement.station == Station.station)
-    # for record in joined_by_station:
-    #     (measurement, station) = record
-
-    #     canonicalized = superhero.replace(" ", "").lower()
-    # for character in justice_league_members:
-    #     search_term = character["superhero"].replace(" ", "").lower()
-
-    #     if search_term == canonicalized:
-    #         return jsonify(character)
 
 
 if __name__ == '__main__':
